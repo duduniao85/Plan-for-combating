9254e0031371d8f0d8ac7d701d124d623175af58
xuyuming <clark_xym@163.com>
2016-05-10 18:44:49 +0800

    新建链家爬虫分析数据

--- lianjiacrawler/main.py
+++ lianjiacrawler/main.py
@@ -1,10 +1,14 @@
 #coding=utf-8
 __author__ = 'xuyuming'
 import secondHandHouseDeal
+import time
+import pandas as pd
 i=1
 itemUrlList=set()
 itemList=[]
-while(True):
+#先模拟登录
+secondHandHouseDeal.lianjialogin()
+while(i<2):
     prefix=r'http://sh.lianjia.com'
     url=r'http://sh.lianjia.com/chengjiao/d'+str(i)
     scndHs4Sale=secondHandHouseDeal.getUrlList(url)
@@ -15,5 +19,13 @@ while(True):
         itemUrl=prefix+item
         itemUrlList.add(itemUrl)
     i+=1
+
+starttime= time.clock()
 for itemUrl in itemUrlList:
+    item= secondHandHouseDeal.getAttr(itemUrl)
+    #print item
     itemList.append(secondHandHouseDeal.getAttr(itemUrl))
+data=pd.DataFrame(itemList)
+endtime=time.clock()
+print endtime-starttime
+print data
