6c475416a3dd78cc6b8060826ee702e934825265
xuyuming <clark_xym@163.com>
2016-05-11 13:29:51 +0800

    新建链家爬虫分析数据

--- lianjiacrawler/main.py
+++ lianjiacrawler/main.py
@@ -4,25 +4,29 @@ import secondHandHouseDeal
 import time
 import pandas as pd
 i=1
-itemUrlList=set()
-itemList=[]
+itemUrlList=set()#房源URL集合
+itemBaseList=[]#房源基础信息LIST，包括ID,URL,签约日期，所属板块
+itemList=[]#成交详情列表
 #先模拟登录
-secondHandHouseDeal.lianjialogin()
-while(i<2):
+#secondHandHouseDeal.lianjialogin()
+#取得需要爬取的ID列表和明细url列表
+while(i<=2):
     prefix=r'http://sh.lianjia.com'
     url=r'http://sh.lianjia.com/chengjiao/d'+str(i)
     scndHs4Sale=secondHandHouseDeal.getUrlList(url)
+    itemBaseList.extend(scndHs4Sale)
     if len(scndHs4Sale)== 0 :
         break
-    #print scndHs4Sale
+    print scndHs4Sale
     for item in scndHs4Sale:
-        itemUrl=prefix+item
+        #print item.get('itemId')
+        itemUrl=prefix+item.get('itemUrl')
         itemUrlList.add(itemUrl)
     i+=1
-
+print itemBaseList
 starttime= time.clock()
 for itemUrl in itemUrlList:
-    item= secondHandHouseDeal.getAttr(itemUrl)
+    item= secondHandHouseDeal.getAttr(itemUrl)#t签约户源详情
     #print item
     itemList.append(secondHandHouseDeal.getAttr(itemUrl))
 data=pd.DataFrame(itemList)
--- lianjiacrawler/secondHandHouseDeal.py
+++ lianjiacrawler/secondHandHouseDeal.py
@@ -2,6 +2,7 @@
 __author__ = 'xuyuming'
 from bs4 import BeautifulSoup
 from urllib import urlopen
+import pandas as pd
 import time
 import HTMLParser
 import urlparse
@@ -19,9 +20,10 @@ def unzip(data):
     data = gz.read()
     gz.close()
     return data
+
 def lianjialogin():
     """
-
+    摸拟链家用户登录
     :param username: 登录用户名
     :param password: 登录密码
     :return:
@@ -36,8 +38,6 @@ def lianjialogin():
     home_url = 'http://sh.lianjia.com/'
     auth_url = 'https://passport.lianjia.com/cas/login?service=http%3A%2F%2Fsh.lianjia.com%2F'
     chengjiao_url = 'http://sh.lianjia.com/chengjiao/'
-
-
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, sdch',
@@ -60,7 +60,7 @@ def lianjialogin():
     # 获取cookie和lt值
     pattern = re.compile(r'JSESSIONID=(.*)')
     jsessionid = pattern.findall(result.info().getheader('Set-Cookie').split(';')[0])[0]
-   # print jsessionid
+    # print jsessionid
     html_content = result.read()
     html_content = unzip(html_content)
     #print html_content.decode('UTF8')
@@ -144,54 +144,79 @@ def lianjialogin():
 def getUrlList(pageUrl):
     """
     传入指定的需要抓取的页面
-    返回需要抓取的明细项url列表
+    返回需要抓取的明细项url列表,以及url列表对应明细项的签约日期
     """
     textdata=urlopen(pageUrl).read()
     soup=BeautifulSoup(textdata,'lxml')
-    searchlist=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > h2 > a')
+    itemUrlList=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > h2 > a')
+    #成交日期
+    itemDealDate=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-2.fr > div > div:nth-of-type(1) > div')
+    #ID
+    itemIdList=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > h2 > a')
+    #区县
+    itemQuxianList=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-1.fl > div.other > div > a:nth-of-type(1)')
+    #片区
+    itemPianquList=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-1.fl > div.other > div > a:nth-of-type(2)')
+    #楼层/层高
+    itemLoucengList=soup.select('body > div.wrapper > div.main-box.clear > div > div.list-wrap > ul > li > div.info-panel > div > div.col-1.fl > div.other > div')
+
     itemList=[]
-    for item in searchlist:
-        itemList.append(item.get('href'))
+    for itemUrl,itemDate,itemId,itemQuxian,itemPianqu,itemLouceng in zip(itemUrlList,itemDealDate,itemIdList,itemQuxianList,itemPianquList,itemLoucengList):
+        temp_strlist=list(itemLouceng.stripped_strings)
+        item = {
+            'itemUrl': itemUrl.get('href'),
+            'signDate': itemDate.get_text(),
+            'itemId': itemId.get('key'),
+            'quxian': itemQuxian.get_text(),
+            'pianqu': itemPianqu.get_text(),
+            'louceng':temp_strlist[3] if (len(temp_strlist) > 3) else '',#不是所有的房子都包含楼层信息，没有则置空
+            'chaoxiang':temp_strlist[5] if (len(temp_strlist) > 5) else ''#不是所有的房子都包含朝向信息，没有则置空
+        }
+        itemList.append(item)
     return itemList
 
 def getAttr(item_page_url):
     """
-    :param item_page_url: 输入明细房子的网页链接
+    :param item_page_url: 输入明细已经成交房子的网页链接
     :return:返回属性值字典列表
     维度：ID，小区名称，户型，区县， 片区， 楼层，总层高 ，朝向 ，建筑年份， 卖点，是否有钥匙，邻近地铁站，装修情况，地址
-    指标：面积，总价，单价，看房人数，同小区挂牌均价
+    指标：面积，总价，单价，同小区挂牌均价
     """
     textdata=urlopen(item_page_url).read()
     soup=BeautifulSoup(textdata,'lxml')
     #############################################维度#####################################################
+    #二手户成交数据ID
     itemId=soup.select('body > div.cj-wrap > div > div.esf-top > div.cj-cun > div.content > div.houseRecord > span.houseNum')[0].get_text().split(u'：')[1]
-    # courtName=soup.select()
-    # huxing=soup.select()
-    # quxian=soup.select()
-    # pianqu=soup.select()
-    # louceng=soup.select()
-    # zongcenggao=soup.select()
-    # chaoxiang=soup.select()
+    #小区名称
+    courtName=soup.findAll("a",{"class":"name-xq"})[0].get_text()
+    #户型说明
+    huxing_shi=soup.select('body > div.cj-wrap > div > div.esf-top > div.cj-cun > div.content > div.houseInfo > div.room > div')[0].get_text().split(' ')[0]
+    huxing_ting=soup.select('body > div.cj-wrap > div > div.esf-top > div.cj-cun > div.content > div.houseInfo > div.room > div')[0].get_text().split(' ')[1]
+    # 建筑年份
     # jianzhunianfen=soup.select()
+    # 卖点
     # maidian=soup.select()
+    # 是否有钥匙
     # youyaoshi=soup.select()
-    # linjindetiezhan=soup.select()
+    #邻近地铁站通过正则表达式提取
+    #linjindetiezhan=soup.select()
+    # 装修情况
     # zhuangxiuqingkuang=soup.select()
+    # 地址
     # dizhi=soup.select()
+    # 面积
     # mianji=soup.select()
+    # 总价
     # zongjia=soup.select()
+    # 单价
     # danjia=soup.select()
-    # kanfangrenshu=soup.select()
+    # 同小区均价
     # tongxiaoqujunjia=soup.select()
 
     item_attr={
             'itemId': itemId,
-            'courtName': '',
-            'huxing': '',
-            'quxian': '',
-            'pianqu': '',
-            'louceng': '',
-            'zongcenggao': '',
+            'courtName': courtName,
+            'huxing': huxing_shi+huxing_ting,
             'chaoxiang': '',
             'jianzhunianfen': '',
             'maidian': '',
@@ -206,8 +231,20 @@ def getAttr(item_page_url):
             'tongxiaoqujunjia': ''
     }
     return item_attr
+def unitTest(itemUrl):
+    """
+
+    :param itemUrl:
+    :return:
+    """
+    textdata=urlopen(itemUrl).read()
+    soup=BeautifulSoup(textdata,'lxml')
+    text=soup.select('body > div.cj-wrap > div > div.esf-top > div.cj-cun > div.content > table > tbody > tr:nth-child(3) > td:nth-child(4)')
+    for a in  list(text.stripped_strings):
+        print a
 
 if __name__ == '__main__':
     #list=getUrlList(r'http://sh.lianjia.com/chengjiao/d20000')
-    print lianjialogin()
+    #unitTest(r'http://sh.lianjia.com/chengjiao/sh1051315.html')
+    getUrlList(r'http://sh.lianjia.com/chengjiao/')
 
